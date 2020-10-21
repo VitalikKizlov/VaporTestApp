88d1de94e25f751e91380fd9eeb84b5d6b4b38f0
Vitalii Kizlov <Vitalii.Kizlov@deltatre.com>
2020-10-21 15:59:00 +0300

    Setup Project
    Add controller

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vitalii Kizlov on 14.10.2020.
+//
+
+import Vapor
+import Fluent
+
+final class Acronym: Model {
+    static let schema = "acronyms"
+    
+    @ID
+    var id: UUID?
+    
+    @Field(key: "short")
+    var short: String
+    @Field(key: "long")
+    var long: String
+    
+    init() {}
+    
+    init(id: UUID? = nil, short: String, long: String) {
+        self.id = id
+        self.short = short
+        self.long = long
+    }
+}
+
+extension Acronym: Content {}
